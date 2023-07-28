994b21ee8a1d656f577dee22ccc13ed6fdd79e57
Mudit200408 <muditpurohit2004.pm@gmail.com>
2023-07-28 19:26:55 +0530

    Add Font Customizations to Display
    Add Font Customizations to Display

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -21,6 +21,7 @@ import android.content.Context;
 import android.os.Bundle;
 
 import com.android.settings.dashboard.DashboardFragment;
+import com.android.settings.development.OverlayCategoryPreferenceController;
 import com.android.settings.display.BrightnessLevelPreferenceController;
 import com.android.settings.display.CameraGesturePreferenceController;
 import com.android.settings.display.LiftToWakePreferenceController;
@@ -85,6 +86,8 @@ public class DisplaySettings extends DashboardFragment {
         controllers.add(new ShowOperatorNamePreferenceController(context));
         controllers.add(new ThemePreferenceController(context));
         controllers.add(new BrightnessLevelPreferenceController(context, lifecycle));
+        controllers.add(new OverlayCategoryPreferenceController(context,
+                "android.theme.customization.font"));
         return controllers;
     }
 
